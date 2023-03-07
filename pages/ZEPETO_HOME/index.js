47a99375bf66e90c95f87394bd2a9a061a05a8b4
dongkyun2331 <dongkyun2331@gmail.com>
2023-03-07 16:25:31 +0900

    a

--- pages/ZEPETO_HOME/index.js
+++ pages/ZEPETO_HOME/index.js
@@ -106,7 +106,91 @@ $(document).ready(function () {
           $(window).on('scroll', function () {
             var windowHeight = $(window).height()
             var scrollHeight = $(document).scrollTop()
-            $('.Frame4_box > ul').each(function () {
+            $('.Frame5_title').each(function () {
+              var elementPosition = $(this).offset().top
+              if (scrollHeight > elementPosition - windowHeight) {
+                $(this).css('opacity', '1').css('transform', 'translateY(0%)')
+              }
+            })
+          })
+        }, 500)
+        setTimeout(function () {
+          $(window).on('scroll', function () {
+            var windowHeight = $(window).height()
+            var scrollHeight = $(document).scrollTop()
+            $('.Frame5_p').each(function () {
+              var elementPosition = $(this).offset().top
+              if (scrollHeight > elementPosition - windowHeight) {
+                $(this).css('opacity', '1').css('transform', 'translateY(0%)')
+              }
+            })
+          })
+        }, 500)
+        setTimeout(function () {
+          $(window).on('scroll', function () {
+            var windowHeight = $(window).height()
+            var scrollHeight = $(document).scrollTop()
+            $('.bul').each(function () {
+              var elementPosition = $(this).offset().top
+              if (scrollHeight > elementPosition - windowHeight) {
+                $(this).css('opacity', '1').css('transform', 'translateY(0%)')
+              }
+            })
+          })
+        }, 500)
+        setTimeout(function () {
+          $(window).on('scroll', function () {
+            var windowHeight = $(window).height()
+            var scrollHeight = $(document).scrollTop()
+            $('.Frame6_title').each(function () {
+              var elementPosition = $(this).offset().top
+              if (scrollHeight > elementPosition - windowHeight) {
+                $(this).css('opacity', '1').css('transform', 'translateY(0%)')
+              }
+            })
+          })
+        }, 500)
+        setTimeout(function () {
+          $(window).on('scroll', function () {
+            var windowHeight = $(window).height()
+            var scrollHeight = $(document).scrollTop()
+            $('.Frame6_p').each(function () {
+              var elementPosition = $(this).offset().top
+              if (scrollHeight > elementPosition - windowHeight) {
+                $(this).css('opacity', '1').css('transform', 'translateY(0%)')
+              }
+            })
+          })
+        }, 500)
+        setTimeout(function () {
+          $(window).on('scroll', function () {
+            var windowHeight = $(window).height()
+            var scrollHeight = $(document).scrollTop()
+            $('.Frame7_title').each(function () {
+              var elementPosition = $(this).offset().top
+              if (scrollHeight > elementPosition - windowHeight) {
+                $(this).css('opacity', '1').css('transform', 'translateY(0%)')
+              }
+            })
+          })
+        }, 500)
+        setTimeout(function () {
+          $(window).on('scroll', function () {
+            var windowHeight = $(window).height()
+            var scrollHeight = $(document).scrollTop()
+            $('.Frame7_p').each(function () {
+              var elementPosition = $(this).offset().top
+              if (scrollHeight > elementPosition - windowHeight) {
+                $(this).css('opacity', '1').css('transform', 'translateY(0%)')
+              }
+            })
+          })
+        }, 500)
+        setTimeout(function () {
+          $(window).on('scroll', function () {
+            var windowHeight = $(window).height()
+            var scrollHeight = $(document).scrollTop()
+            $('.Frame7 > button').each(function () {
               var elementPosition = $(this).offset().top
               if (scrollHeight > elementPosition - windowHeight) {
                 $(this).css('opacity', '1').css('transform', 'translateY(0%)')
