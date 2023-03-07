60819ca68d68ed183a28c9253b6fafb866afa54d
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-03-07 20:57:56 +0900

    a

--- pages/busan/js/app.js
+++ pages/busan/js/app.js
@@ -11,14 +11,17 @@ $(document).ready(function () {
     dots: true,
     autoplay: true,
     autoplaySpeed: 3000,
-    nextArrow: $('.right'),
-    prevArrow: $('.left'),
     draggable: false,
   })
 
   $('.slide_slick').slick({
     nextArrow: $('.right2'),
     prevArrow: $('.left2'),
+  })
+
+  $('.gray_slick').slick({
+    nextArrow: $('.right3'),
+    prevArrow: $('.left3'),
   })
 
   $('.right').click(function () {
