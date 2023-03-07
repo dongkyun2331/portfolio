00dff7564aefc4830ffa8b31f4987ebeb3bcf6f8
dongkyun2331 <dongkyun2331@gmail.com>
2023-03-07 17:28:43 +0900

    a

--- pages/ZEPETO_HOME/sub.js
+++ pages/ZEPETO_HOME/sub.js
@@ -10,5 +10,49 @@ $(document).ready(function () {
     setTimeout(function () {
       $('.Frame2_p').css('opacity', '1').css('transform', 'translateY(0%)')
     }, 400)
+    setTimeout(function () {
+      $('.Frame2_support')
+        .css('opacity', '1')
+        .css('transform', 'translateY(0%)')
+    }, 800)
+    setTimeout(function () {
+      $('.Frame2_ul').css('opacity', '1').css('transform', 'translateY(0%)')
+    }, 1200)
+    setTimeout(function () {
+      $(window).on('scroll', function () {
+        var windowHeight = $(window).height()
+        var scrollHeight = $(document).scrollTop()
+        $('.Frame3_h2').each(function () {
+          var elementPosition = $(this).offset().top
+          if (scrollHeight > elementPosition - windowHeight) {
+            $(this).css('opacity', '1').css('transform', 'translateY(0%)')
+          }
+        })
+      })
+    }, 500)
+    setTimeout(function () {
+      $(window).on('scroll', function () {
+        var windowHeight = $(window).height()
+        var scrollHeight = $(document).scrollTop()
+        $('.Frame3_ul').each(function () {
+          var elementPosition = $(this).offset().top
+          if (scrollHeight > elementPosition - windowHeight) {
+            $(this).css('opacity', '1').css('transform', 'translateY(0%)')
+          }
+        })
+      })
+    }, 500)
+    setTimeout(function () {
+      $(window).on('scroll', function () {
+        var windowHeight = $(window).height()
+        var scrollHeight = $(document).scrollTop()
+        $('.Frame3_img').each(function () {
+          var elementPosition = $(this).offset().top
+          if (scrollHeight > elementPosition - windowHeight) {
+            $(this).css('opacity', '1').css('transform', 'translateY(0%)')
+          }
+        })
+      })
+    }, 500)
   })
 })
