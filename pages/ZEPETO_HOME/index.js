8f16223bc298776729bc82e353d502329fbdb7e2
dongkyun2331 <dongkyun2331@gmail.com>
2023-03-14 13:56:56 +0900

    q

--- pages/ZEPETO_HOME/index.js
+++ pages/ZEPETO_HOME/index.js
@@ -6,7 +6,7 @@ $('.bul').slick({
   slidesToShow: 5,
 })
 
-document.body.style.zoom = '150%'
+// document.body.style.zoom = '150%'
 
 $(document).ready(function () {
   $('.Frame2_title').css('opacity', '1').css('transform', 'translateY(0%)')
