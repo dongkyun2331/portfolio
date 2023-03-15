4af259c274f76aef54bb9b2eddce7eb0e8f8310a
dongkyun2331 <dongkyun2331@gmail.com>
2023-03-15 16:23:26 +0900

    a

--- pages/busan/js/app.js
+++ pages/busan/js/app.js
@@ -1,60 +1,54 @@
-$(document).ready(function () {
-  $('.wbox_01ul').slick({
-    vertical: true,
-    draggable: false,
-    dots: false,
-    arrows: false,
-  })
-
-  $('.bul').slick({
-    draggable: false,
-    dots: false,
-    arrows: false,
-  })
-
-  $('.slide_slick').slick({
-    nextArrow: $('.right2'),
-    prevArrow: $('.left2'),
-  })
-
-  $('.photo_slick').slick({
-    nextArrow: $('.right3'),
-    prevArrow: $('.left3'),
-  })
-
-  $('.right').click(function () {
-    $('.wbox_01ul').slick('slickNext')
-    $('.bul').slick('slickNext')
-  })
-
-  $('.left').click(function () {
-    $('.wbox_01ul').slick('slickPrev')
-    $('.bul').slick('slickPrev')
-  })
-
-  var intervalId = null // setInterval() 함수의 반환 값
-
-  $('.start').click(function () {
-    // setInterval() 함수로 일정 간격으로 코드를 반복 실행
-    intervalId = setInterval(function () {
-      $('.wbox_01ul').slick('slickNext')
-      $('.bul').slick('slickNext')
-    }, 3000) // 3초 간격으로 실행
-  })
-
-  $('.stop').click(function () {
-    clearInterval(intervalId) // setInterval() 함수를 중지
-  })
+$('.wbox_01ul').slick({
+  vertical: true,
+  draggable: false,
+  dots: false,
+  arrows: false,
+  pauseOnHover: false,
 })
 
-$(document).ready(function () {
-  $('.y1').magnificPopup({
-    disableOn: 700,
-    type: 'iframe',
-    mainClass: 'mfp-fade',
-    removalDelay: 160,
-    preloader: false,
+$('.bul').slick({
+  draggable: false,
+  dots: false,
+  arrows: false,
+  pauseOnHover: false,
+})
 
-    fixedContentPos: false,
-  })
+$('.slide_slick').slick({
+  nextArrow: $('.right2'),
+  prevArrow: $('.left2'),
+})
+
+$('.photo_slick').slick({
+  nextArrow: $('.right3'),
+  prevArrow: $('.left3'),
+})
+
+$('.right').click(function () {
+  $('.wbox_01ul').slick('slickNext')
+  $('.bul').slick('slickNext')
+})
+
+$('.left').click(function () {
+  $('.wbox_01ul').slick('slickPrev')
+  $('.bul').slick('slickPrev')
+})
+
+$('.start').click(function () {
+  $('.wbox_01ul').slick('slickPlay')
+  $('.bul').slick('slickPlay')
+})
+
+$('.stop').click(function () {
+  $('.wbox_01ul').slick('slickPause')
+  $('.bul').slick('slickPause')
+})
+
+$('.y1').magnificPopup({
+  disableOn: 700,
+  type: 'iframe',
+  mainClass: 'mfp-fade',
+  removalDelay: 160,
+  preloader: false,
+
+  fixedContentPos: false,
 })
