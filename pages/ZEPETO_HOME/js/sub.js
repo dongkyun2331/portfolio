e58d521bb4a83398630269002544a4741e4ef900
dongkyun2331 <dongkyun2331@gmail.com>
2023-03-16 15:23:20 +0900

    a

--- pages/ZEPETO_HOME/js/sub.js
+++ pages/ZEPETO_HOME/js/sub.js
@@ -4,115 +4,70 @@ $('.Frame4_slick_all').slick({
   prevArrow: $('.prev'),
 })
 
-$(document).ready(function () {
-  setTimeout(function () {
-    $('.Frame2_h1').css('opacity', '1').css('transform', 'translateY(0%)')
-    setTimeout(function () {
-      $('.Frame2_p').css('opacity', '1').css('transform', 'translateY(0%)')
-    }, 300)
-    setTimeout(function () {
-      $('.Frame2_support')
-        .css('opacity', '1')
-        .css('transform', 'translateY(0%)')
-    }, 600)
-    setTimeout(function () {
-      $('.Frame2_ul').css('opacity', '1').css('transform', 'translateY(0%)')
-    }, 900)
-    setTimeout(function () {
-      $(window).on('scroll', function () {
-        var windowHeight = $(window).height()
-        var scrollHeight = $(document).scrollTop()
-        $('.Frame3_h2').each(function () {
-          var elementPosition = $(this).offset().top
-          if (scrollHeight > elementPosition - windowHeight) {
-            $(this).css('opacity', '1').css('transform', 'translateY(0%)')
-          }
-        })
-      })
-    }, 500)
-    setTimeout(function () {
-      $(window).on('scroll', function () {
-        var windowHeight = $(window).height()
-        var scrollHeight = $(document).scrollTop()
-        $('.Frame3_ul').each(function () {
-          var elementPosition = $(this).offset().top
-          if (scrollHeight > elementPosition - windowHeight) {
-            $(this).css('opacity', '1').css('transform', 'translateY(0%)')
-          }
-        })
-      })
-    }, 500)
-    setTimeout(function () {
-      $(window).on('scroll', function () {
-        var windowHeight = $(window).height()
-        var scrollHeight = $(document).scrollTop()
-        $('.Frame3_img').each(function () {
-          var elementPosition = $(this).offset().top
-          if (scrollHeight > elementPosition - windowHeight) {
-            $(this).css('opacity', '1').css('transform', 'translateY(0%)')
-          }
-        })
-      })
-    }, 500)
-    setTimeout(function () {
-      $(window).on('scroll', function () {
-        var windowHeight = $(window).height()
-        var scrollHeight = $(document).scrollTop()
-        $('.Frame4_h2').each(function () {
-          var elementPosition = $(this).offset().top
-          if (scrollHeight > elementPosition - windowHeight) {
-            $(this).css('opacity', '1').css('transform', 'translateY(0%)')
-          }
-        })
-      })
-    }, 500)
-    setTimeout(function () {
-      $(window).on('scroll', function () {
-        var windowHeight = $(window).height()
-        var scrollHeight = $(document).scrollTop()
-        $('.Frame4_p').each(function () {
-          var elementPosition = $(this).offset().top
-          if (scrollHeight > elementPosition - windowHeight) {
-            $(this).css('opacity', '1').css('transform', 'translateY(0%)')
-          }
-        })
-      })
-    }, 500)
-    setTimeout(function () {
-      $(window).on('scroll', function () {
-        var windowHeight = $(window).height()
-        var scrollHeight = $(document).scrollTop()
-        $('.Frame5_h2').each(function () {
-          var elementPosition = $(this).offset().top
-          if (scrollHeight > elementPosition - windowHeight) {
-            $(this).css('opacity', '1').css('transform', 'translateY(0%)')
-          }
-        })
-      })
-    }, 500)
-    setTimeout(function () {
-      $(window).on('scroll', function () {
-        var windowHeight = $(window).height()
-        var scrollHeight = $(document).scrollTop()
-        $('.Frame6_h2').each(function () {
-          var elementPosition = $(this).offset().top
-          if (scrollHeight > elementPosition - windowHeight) {
-            $(this).css('opacity', '1').css('transform', 'translateY(0%)')
-          }
-        })
-      })
-    }, 500)
-    setTimeout(function () {
-      $(window).on('scroll', function () {
-        var windowHeight = $(window).height()
-        var scrollHeight = $(document).scrollTop()
-        $('.Frame6_rectangle').each(function () {
-          var elementPosition = $(this).offset().top
-          if (scrollHeight > elementPosition - windowHeight) {
-            $(this).css('opacity', '1').css('transform', 'translateY(0%)')
-          }
-        })
-      })
-    }, 500)
-  })
+ScrollReveal().reveal('.Frame2_h1', {
+  duration: 1000,
+  distance: '60px',
+  origin: 'bottom',
+})
+ScrollReveal().reveal('.Frame2_p', {
+  delay: 300,
+  duration: 1000,
+  distance: '60px',
+  origin: 'bottom',
+})
+ScrollReveal().reveal('.Frame2_support', {
+  delay: 600,
+  duration: 1000,
+  distance: '60px',
+  origin: 'bottom',
+})
+ScrollReveal().reveal('.Frame2_ul', {
+  delay: 900,
+  duration: 1000,
+  distance: '60px',
+  origin: 'bottom',
+})
+ScrollReveal().reveal('.Frame3_h2', {
+  duration: 1000,
+  distance: '60px',
+  origin: 'bottom',
+})
+ScrollReveal().reveal('.Frame3_ul', {
+  delay: 300,
+  duration: 1000,
+  distance: '60px',
+  origin: 'bottom',
+})
+ScrollReveal().reveal('.Frame3_img', {
+  delay: 600,
+  duration: 1000,
+  distance: '60px',
+  origin: 'bottom',
+})
+ScrollReveal().reveal('.Frame4_h2', {
+  duration: 1000,
+  distance: '60px',
+  origin: 'bottom',
+})
+ScrollReveal().reveal('.Frame4_p', {
+  delay: 300,
+  duration: 1000,
+  distance: '60px',
+  origin: 'bottom',
+})
+ScrollReveal().reveal('.Frame5_h2', {
+  duration: 1000,
+  distance: '60px',
+  origin: 'bottom',
+})
+ScrollReveal().reveal('.Frame6_h2', {
+  duration: 1000,
+  distance: '60px',
+  origin: 'bottom',
+})
+ScrollReveal().reveal('.Frame6_rectangle', {
+  delay: 300,
+  duration: 1000,
+  distance: '60px',
+  origin: 'bottom',
 })
