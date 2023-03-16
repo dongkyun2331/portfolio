77fd2e7b4c8ebc0e49fec52912206a9d2cbed7a3
dongkyun2331 <dongkyun2331@gmail.com>
2023-03-16 15:01:49 +0900

    a

--- pages/ZEPETO_HOME/index.js
+++ pages/ZEPETO_HOME/index.js
@@ -8,78 +8,110 @@ $('.bul').slick({
 
 // document.body.style.zoom = '150%'
 ScrollReveal().reveal('.Frame2_title', {
+  duration: 1000,
   distance: '60px',
   origin: 'bottom',
 })
 ScrollReveal().reveal('.btn-link', {
+  delay: 500,
+  duration: 1000,
   distance: '60px',
   origin: 'bottom',
 })
 ScrollReveal().reveal('.Frame3_title', {
+  duration: 1000,
   distance: '60px',
   origin: 'bottom',
 })
 ScrollReveal().reveal('.Frame3_p', {
+  delay: 500,
+  duration: 1000,
   distance: '60px',
   origin: 'bottom',
 })
 ScrollReveal().reveal('.Frame3_img', {
+  delay: 1000,
+  duration: 1000,
   distance: '60px',
   origin: 'bottom',
 })
 ScrollReveal().reveal('.Frame3_ul', {
+  delay: 1000,
+  duration: 1000,
   distance: '60px',
   origin: 'bottom',
 })
 ScrollReveal().reveal('.Frame4_title', {
+  duration: 1000,
   distance: '60px',
   origin: 'bottom',
 })
 ScrollReveal().reveal('..Frame4_p', {
+  delay: 500,
+  duration: 1000,
   distance: '60px',
   origin: 'bottom',
 })
 ScrollReveal().reveal('.Frame4_box > ul', {
+  delay: 1000,
+  duration: 1000,
   distance: '60px',
   origin: 'bottom',
 })
 ScrollReveal().reveal('.Frame5_title', {
+  duration: 1000,
   distance: '60px',
   origin: 'bottom',
 })
 ScrollReveal().reveal('.Frame5_p', {
+  delay: 500,
+  duration: 1500,
   distance: '60px',
   origin: 'bottom',
 })
 ScrollReveal().reveal('.bul', {
+  delay: 1000,
+  duration: 1500,
   distance: '60px',
   origin: 'bottom',
 })
 ScrollReveal().reveal('.Frame6_title', {
+  duration: 1000,
   distance: '60px',
   origin: 'bottom',
 })
 ScrollReveal().reveal('.Frame6_p', {
+  delay: 500,
+  duration: 1000,
   distance: '60px',
   origin: 'bottom',
 })
 ScrollReveal().reveal('.Frame6_box > video', {
+  delay: 1000,
+  duration: 1000,
   distance: '60px',
   origin: 'bottom',
 })
 ScrollReveal().reveal('.Frame7_title', {
+  duration: 1000,
   distance: '60px',
   origin: 'bottom',
 })
 ScrollReveal().reveal('.Frame7_p', {
+  delay: 500,
+  duration: 1000,
   distance: '60px',
   origin: 'bottom',
 })
 ScrollReveal().reveal('.Frame7 > button', {
+  delay: 1000,
+  duration: 1000,
   distance: '60px',
   origin: 'bottom',
 })
 ScrollReveal().reveal('.Frame7_rg', {
+  delay: 1000,
+  duration: 1000,
   distance: '60px',
   origin: 'bottom',
 })
