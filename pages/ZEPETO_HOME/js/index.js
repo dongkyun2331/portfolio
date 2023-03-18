a8783769f01778de23a4c721508c56ac7c828af4
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-03-18 20:21:54 +0900

    a

--- pages/ZEPETO_HOME/js/index.js
+++ pages/ZEPETO_HOME/js/index.js
@@ -1,117 +1,118 @@
-$('.bul').slick({
+$(".bul").slick({
   autoplay: true,
   centerMode: true,
   adaptiveHeight: true,
   draggable: true,
   slidesToShow: 5,
-})
+  pauseOnHover: false,
+});
 
 // document.body.style.zoom = '150%'
-ScrollReveal().reveal('.Frame2_title', {
+ScrollReveal().reveal(".Frame2_title", {
   duration: 1000,
-  distance: '60px',
-  origin: 'bottom',
-})
-ScrollReveal().reveal('.btn-link', {
+  distance: "60px",
+  origin: "bottom",
+});
+ScrollReveal().reveal(".btn-link", {
   delay: 500,
   duration: 1000,
-  distance: '60px',
-  origin: 'bottom',
-})
-ScrollReveal().reveal('.Frame3_title', {
+  distance: "60px",
+  origin: "bottom",
+});
+ScrollReveal().reveal(".Frame3_title", {
   duration: 1000,
-  distance: '60px',
-  origin: 'bottom',
-})
-ScrollReveal().reveal('.Frame3_p', {
+  distance: "60px",
+  origin: "bottom",
+});
+ScrollReveal().reveal(".Frame3_p", {
   delay: 500,
   duration: 1000,
-  distance: '60px',
-  origin: 'bottom',
-})
-ScrollReveal().reveal('.Frame3_img', {
+  distance: "60px",
+  origin: "bottom",
+});
+ScrollReveal().reveal(".Frame3_img", {
   delay: 1000,
   duration: 1000,
-  distance: '60px',
-  origin: 'bottom',
-})
-ScrollReveal().reveal('.Frame3_ul', {
+  distance: "60px",
+  origin: "bottom",
+});
+ScrollReveal().reveal(".Frame3_ul", {
   delay: 1000,
   duration: 1000,
-  distance: '60px',
-  origin: 'bottom',
-})
-ScrollReveal().reveal('.Frame4_title', {
+  distance: "60px",
+  origin: "bottom",
+});
+ScrollReveal().reveal(".Frame4_title", {
   duration: 1000,
-  distance: '60px',
-  origin: 'bottom',
-})
-ScrollReveal().reveal('..Frame4_p', {
+  distance: "60px",
+  origin: "bottom",
+});
+ScrollReveal().reveal("..Frame4_p", {
   delay: 500,
   duration: 1000,
-  distance: '60px',
-  origin: 'bottom',
-})
-ScrollReveal().reveal('.Frame4_box > ul', {
+  distance: "60px",
+  origin: "bottom",
+});
+ScrollReveal().reveal(".Frame4_box > ul", {
   delay: 1000,
   duration: 1000,
-  distance: '60px',
-  origin: 'bottom',
-})
-ScrollReveal().reveal('.Frame5_title', {
+  distance: "60px",
+  origin: "bottom",
+});
+ScrollReveal().reveal(".Frame5_title", {
   duration: 1000,
-  distance: '60px',
-  origin: 'bottom',
-})
-ScrollReveal().reveal('.Frame5_p', {
+  distance: "60px",
+  origin: "bottom",
+});
+ScrollReveal().reveal(".Frame5_p", {
   delay: 500,
   duration: 1500,
-  distance: '60px',
-  origin: 'bottom',
-})
-ScrollReveal().reveal('.bul', {
+  distance: "60px",
+  origin: "bottom",
+});
+ScrollReveal().reveal(".bul", {
   delay: 1000,
   duration: 1500,
-  distance: '60px',
-  origin: 'bottom',
-})
-ScrollReveal().reveal('.Frame6_title', {
+  distance: "60px",
+  origin: "bottom",
+});
+ScrollReveal().reveal(".Frame6_title", {
   duration: 1000,
-  distance: '60px',
-  origin: 'bottom',
-})
-ScrollReveal().reveal('.Frame6_p', {
+  distance: "60px",
+  origin: "bottom",
+});
+ScrollReveal().reveal(".Frame6_p", {
   delay: 500,
   duration: 1000,
-  distance: '60px',
-  origin: 'bottom',
-})
-ScrollReveal().reveal('.Frame6_box > video', {
+  distance: "60px",
+  origin: "bottom",
+});
+ScrollReveal().reveal(".Frame6_box > video", {
   delay: 1000,
   duration: 1000,
-  distance: '60px',
-  origin: 'bottom',
-})
-ScrollReveal().reveal('.Frame7_title', {
+  distance: "60px",
+  origin: "bottom",
+});
+ScrollReveal().reveal(".Frame7_title", {
   duration: 1000,
-  distance: '60px',
-  origin: 'bottom',
-})
-ScrollReveal().reveal('.Frame7_p', {
+  distance: "60px",
+  origin: "bottom",
+});
+ScrollReveal().reveal(".Frame7_p", {
   delay: 500,
   duration: 1000,
-  distance: '60px',
-  origin: 'bottom',
-})
-ScrollReveal().reveal('.Frame7 > button', {
+  distance: "60px",
+  origin: "bottom",
+});
+ScrollReveal().reveal(".Frame7 > button", {
   delay: 1000,
   duration: 1000,
-  distance: '60px',
-  origin: 'bottom',
-})
-ScrollReveal().reveal('.Frame7_rg', {
+  distance: "60px",
+  origin: "bottom",
+});
+ScrollReveal().reveal(".Frame7_rg", {
   delay: 1000,
   duration: 1000,
-  distance: '60px',
-  origin: 'bottom',
-})
+  distance: "60px",
+  origin: "bottom",
+});
